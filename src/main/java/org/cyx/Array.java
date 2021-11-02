a1790fdd7a9547fb1960dd2f5df910c06b6fff07
Joker-Cyx <614837598@qq.com>
2021-11-03 00:26:48 +0800

    【Leetcode】 DP 练习-2

--- src/main/java/org/cyx/Array.java
+++ src/main/java/org/cyx/Array.java
@@ -26,7 +26,6 @@ public class Array {
 
     public static void main(String[] args) {
 
-        printTitle("11. 盛最多水的容器");
         /**
          * 11. 盛最多水的容器
          * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点(i,ai) 。
@@ -34,10 +33,10 @@ public class Array {
          * @sin 2021.10.28 23:27
          * @end 2021.10.29 00:19
          */
+        printTitle("11. 盛最多水的容器");
         int[] height11 = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
         System.out.println(new MaxArea().maxArea(height11));
 
-        printTitle("26. 删除有序数组中的重复项");
         /**
          * 26. 删除有序数组中的重复项
          * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
@@ -45,35 +44,64 @@ public class Array {
          * @sin
          * @end
          */
+        printTitle("26. 删除有序数组中的重复项");
         int[] nums26 = new int[]{0};
         System.out.println(new RemoveDuplicates().removeDuplicates(nums26));
 
-        printTitle("62. 不同路径");
+        /**
+         * 45. 跳跃游戏 II
+         * 给你一个非负整数数组nums ，你最初位于数组的第一个位置。
+         * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+         * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+         * 假设你总是可以到达数组的最后一个位置。
+         * @sin 2021.11.02 23:50
+         * @end 2021.11.03 00:26
+         */
+        printTitle("45. 跳跃游戏 II");
+        int[] nums451 = new int[]{2, 3, 1, 1, 4};
+        int[] nums452 = new int[]{2, 3, 0, 1, 4};
+        System.out.println(new Jump().jump(nums451));
+
+        /**
+         * 55. 跳跃游戏
+         * 给定一个非负整数数组 nums ，你最初位于数组的 第一个下标 。
+         * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+         * 判断你是否能够到达最后一个下标。
+         * @sin 2021.11.02 23:25
+         * @end 2021.11.02 23:44
+         */
+        printTitle("55. 跳跃游戏");
+        int[] nums551 = new int[]{3, 2, 1, 0, 1};
+        int[] nums552 = new int[]{2, 3, 1, 1, 4};
+        System.out.println(new CanJump().canJump(nums551));
+
         /**
          * 62. 不同路径
          * 一个机器人位于一个 m x n网格的左上角 （起始点在下图中标记为 “Start” ）。
          * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
          * 问总共有多少条不同的路径？
+         * O(mn), O(mn)，传统 DP 方案空间复杂度可优化至O(min(m,n))
          * @sin 2021.11.01 23:53
          * @end 2021.11.02 00：12
          */
+        printTitle("62. 不同路径");
         System.out.println(new UniquePaths().uniquePaths(3, 7));
 
-        printTitle("63. 不同路径 II");
         /**
          * 63. 不同路径 II
          * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
          * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
          * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
          * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+         * O(mn), O(mn)，传统 DP 方案空间复杂度可优化至O(min(m,n))
          * @sin 2021.11.02 00：15
          * @end 2021.11.02 00：29
          */
+        printTitle("63. 不同路径 II");
         int[][] obstacleGrid631 = new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
         int[][] obstacleGrid632 = new int[][]{{0, 1, 0}, {1, 0, 0}, {0, 0, 0}};
         System.out.println(new UniquePathsWithObstacles().uniquePathsWithObstacles(obstacleGrid632));
 
-        printTitle("66. 加一");
         /**
          * 66. 加一
          * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
@@ -82,12 +110,14 @@ public class Array {
          * @sin
          * @end
          */
+        printTitle("66. 加一");
         int[] digits66 = new int[]{9, 9, 9, 9};
         Arrays.stream(new PlusOne().plusOne(digits66)).forEach(System.out::println);
 
 
     }
 
+    // help print title
     static void printTitle(String title) {
         int len = title.length();
         int left = (118 - len) / 2;
@@ -144,6 +174,41 @@ class RemoveDuplicates {
         return l + 1;
     }
 }
+
+// 45. 跳跃游戏 II
+class Jump {
+    public int jump(int[] nums) {
+        int maxPosNextStep = 0;
+        int steps = 0;
+        int maxPosCurrStep = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxPosNextStep = Math.max(i + nums[i], maxPosNextStep);
+            if (maxPosNextStep >= nums.length - 1) {
+                return steps + 1;
+            }
+            if (i == maxPosCurrStep) {
+                maxPosCurrStep = maxPosNextStep;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
+
+// 55. 跳跃游戏
+class CanJump {
+    public boolean canJump(int[] nums) {
+        int maxCan = 0;
+        for (int i = 0; i < nums.length && i <= maxCan; i++) {
+            maxCan = Math.max(maxCan, i + nums[i]);
+            if (maxCan >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
 
 // 62. 不同的路径
 class UniquePaths {
