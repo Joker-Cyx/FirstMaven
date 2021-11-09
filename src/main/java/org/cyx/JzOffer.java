bbd019582a53b8af17a47b05dc0d11d0fdb4349d
Joker-Cyx <614837598@qq.com>
2021-11-10 00:54:09 +0800

    【Leetcode】 JzOffer - 3

--- src/main/java/org/cyx/JzOffer.java
+++ src/main/java/org/cyx/JzOffer.java
@@ -2,6 +2,9 @@ package org.cyx;
 
 import org.cyx.util.PrintUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class JzOffer {
@@ -30,6 +33,22 @@ public class JzOffer {
          * @end 2021.11.08 23:50
          */
         PrintUtil.printTitle("JZ30 包含min函数的栈");
+
+        /**
+         * JZ51 数组中的逆序对
+         * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
+         * 输入一个数组,求出这个数组中的逆序对的总数P。并将P对1000000007取模的结果输出。 即输出P mod 1000000007
+         * 要求：空间复杂度 O(n)，时间复杂度 O(nlogn)
+         * @sin 2021.11.09 23:50
+         * @end 2021.11.10 00:42
+         */
+        PrintUtil.printTitle("JZ51 数组中的逆序对");
+        int[] nums = new int[]{364, 637, 341, 406, 747, 995, 234, 971, 571, 219, 993, 407, 416, 366, 315, 301, 601, 650,
+                418, 355, 460, 505, 360, 965, 516, 648, 727, 667, 465, 849, 455, 181, 486, 149, 588, 233, 144, 174, 557,
+                67, 746, 550, 474, 162, 268, 142, 463, 221, 882, 576, 604, 739, 288, 569, 256, 936, 275, 401, 497, 82,
+                935, 983, 583, 523, 697, 478, 147, 795, 380, 973, 958, 115, 773, 870, 259, 655, 446, 863, 735, 784, 3,
+                671, 433, 630, 425, 930, 64, 266, 235, 187, 284, 665, 874, 80, 45, 848, 38, 811, 267, 575};
+        System.out.println(new InversePairs().inversePairs(nums));
 
 
         /**
@@ -64,14 +83,14 @@ class StackToQueue {
     }
 }
 
-//JZ30 包含min函数的栈
-public class StackWithMinMethod {
-    private Stack<Integer> norStk = new Stack<>();
-    private Stack<Integer> minStk = new Stack<>();
+// JZ30 包含min函数的栈
+class StackWithMinMethod {
+    private final Stack<Integer> norStk = new Stack<>();
+    private final Stack<Integer> minStk = new Stack<>();
 
     public void push(int node) {
         norStk.push(node);
-        if(minStk.empty()) {
+        if (minStk.empty()) {
             minStk.push(node);
         } else {
             minStk.push(Math.min(minStk.peek(), node));
@@ -94,12 +113,66 @@ public class StackWithMinMethod {
     }
 }
 
+// JZ51 数组中的逆序对
+class InversePairs {
+
+    private int numOfInvPairs = 0;
+
+    // 记录左节点个数的最小堆
+    class BstNode {
+        int val;
+        int leftNum;
+        BstNode left;
+        BstNode right;
+
+        public BstNode(int v) {
+            this.val = v;
+            this.leftNum = 0;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    private void addBstNode(BstNode root, BstNode node) {
+        if (node.val <= root.val) {
+            root.leftNum++;
+            if (root.left != null) {
+                addBstNode(root.left, node);
+            } else {
+                root.left = node;
+            }
+        } else {
+            numOfInvPairs = (numOfInvPairs + root.leftNum + 1) % 1000000007;
+            if (root.right != null) {
+                addBstNode(root.right, node);
+            } else {
+                root.right = node;
+            }
+        }
+    }
+
+    public int inversePairs(int[] array) {
+        if (array.length <= 1) {
+            return 0;
+        }
+        List<BstNode> listBstNode = new ArrayList<>();
+//        Arrays.stream(array).forEach(i -> listBstNode.add(new BstNode(i)));
+        for (int j : array) {
+            listBstNode.add(new BstNode(j));
+        }
+        for (int i = listBstNode.size() - 2; i >= 0; i--) {
+            addBstNode(listBstNode.get(listBstNode.size() - 1), listBstNode.get(i));
+        }
+        return this.numOfInvPairs;
+    }
+}
+
 // JZ73 翻转单词序列
 class ReverseSentence {
     public String reverseSentence(String str) {
         String[] s = str.split(" ");
         StringBuilder ret = new StringBuilder();
-        for(int i = s.length - 1; i > 0; i--) {
+        for (int i = s.length - 1; i > 0; i--) {
             ret.append(s[i]).append(" ");
         }
         return ret + s[0];
